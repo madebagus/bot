11c1f4cd1038734dca78cb4bdb29ba80a9d58434
madebagus <55092930+madebagus@users.noreply.github.com>
2024-12-06 12:00:06 +0700

    adding status monitoring message every 5 minutes

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 from threading import Thread
 import time
+import requests
 from flask import Flask
 from binance_bot.bot.new_bot_new_no_sltp import run_trading_bot_task
 from binance_bot.reversal.reversal_monitor_new_no_sltp import monitor_positions
 from binance_bot.data.database_management import fetch_and_upsert_order_history
+from binance_bot.messaging.chat_bot import send_status_message
 
 # Initialize the Flask application
 app = Flask(__name__)
 
-# Register the blueprints with the Flask app
-# app.register_blueprint(run_bot_real_bp)
+# Replace with your Telegram Bot Token and Chat ID
+BOT_TOKEN = "YOUR_BOT_TOKEN"
+CHAT_ID = "YOUR_CHAT_ID"
+
+
+
+# Function to periodically send the status message
+def start_status_scheduler():
+    print("Starting Status Scheduler...")
+    while True:
+        send_status_message()
+        time.sleep(300)  # Send every 30 minutes (1800 seconds)
 
 # Function to run the reversal monitor in a separate thread
 def start_profit_monitor():
@@ -42,6 +54,10 @@ if __name__ == '__main__':
     bot_thread = Thread(target=start_bot)
     bot_thread.daemon = True  # Daemon thread will exit when the main program exits
     bot_thread.start()
+
+    status_thread = Thread(target=start_status_scheduler)
+    status_thread.daemon = True  # Daemon thread will exit when the main program exits
+    status_thread.start()
 
     # Run the Flask app (it will not block background threads)
     app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=False)  # `use_reloader=False` to prevent running the background tasks multiple times
--- binance_bot/messaging/chat_bot.py
+++ binance_bot/messaging/chat_bot.py
@@ -20,3 +20,21 @@ def send_telegram_message(message):
         print("Telegram message sent successfully!")
     else:
         print(f"Failed to send message: {response.status_code}, {response.text}")
+
+
+
+# Function to send a status message to Telegram
+def send_status_message():
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": "...I Am Alive..."
+    }
+    try:
+        response = requests.post(url, json=payload)
+        if response.status_code == 200:
+            print("Status message sent successfully!")
+        else:
+            print(f"Failed to send status message: {response.text}")
+    except Exception as e:
+        print(f"Error sending status message: {e}")
