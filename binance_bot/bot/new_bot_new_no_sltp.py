7869617cda81b96eee3ea322b8a81db97d72708d
madebagus <55092930+madebagus@users.noreply.github.com>
2024-12-06 23:26:08 +0700

    some change

--- binance_bot/bot/new_bot_new_no_sltp.py
+++ binance_bot/bot/new_bot_new_no_sltp.py
@@ -445,7 +445,7 @@ def place_futures_order(symbol, trend, leverage, quantity, entry_price, usdt_to_
 import threading
 
 # Initialize the list of coin pairs with a default value
-coin_pairs = ['BCHUSDT','ATOMUSDT','DOTUSDT','LTCUSDT','XMRUSDT','ETHUSDT','INJUSDT','XRPUSDT','BNBUSDT','SOLUSDT']  # Example with 4 pairs
+coin_pairs = ['BCHUSDT','DOTUSDT','LTCUSDT','XMRUSDT','ETHUSDT','INJUSDT','XRPUSDT','BNBUSDT','SUIUSDT']  # Example with 4 pairs
 # Flag to control whether the fetch_recent_orders task should be canceled
 cancel_fetch_orders = False
 # Function to handle trading for each symbol
