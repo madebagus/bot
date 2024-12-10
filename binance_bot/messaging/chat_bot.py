33de188c0c6b75cc2d3aa472b5cd88fa3656bee3
madebagus <55092930+madebagus@users.noreply.github.com>
2024-12-10 13:48:00 +0700

    change bot version

--- binance_bot/messaging/chat_bot.py
+++ binance_bot/messaging/chat_bot.py
@@ -28,7 +28,7 @@ def send_status_message():
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
     payload = {
         "chat_id": CHAT_ID,
-        "text": "...I Am Alive [v2.2.1]..."
+        "text": "...I Am Alive [v2.2.2]..."
     }
     try:
         response = requests.post(url, json=payload)
